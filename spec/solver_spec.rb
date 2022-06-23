dc6a3f50a70d9f1cf9aa3570279e855a2aa66275
Cédric Kossi <lionel.kossi@gmail.com>
2022-06-23 19:10:22 +0000

    Add test for reversed method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,8 +2,20 @@ require_relative '../solver'
 
 describe "factorial method" do
   it 'take one argument and returns its factorial' do
-    factorial = factorial(3)
+    fact = factorial(3)
 
-    expect(factorial).to eq(6)
+    expect(fact).to eq(6)
   end
+end
+
+describe 'reverse method' do
+  it 'take a string and return it reversed' do
+    rev = reversed('world')
+
+    expect(rev).to eq(rev)
+  end
+end
+
+describe '' do
+
 end
