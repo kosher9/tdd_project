7afd3f3f70a117b0ee59fc8fb381ac01567f48f4
Cédric Kossi <lionel.kossi@gmail.com>
2022-06-24 09:18:09 +0000

    Clean code

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,43 +1,46 @@
 require_relative '../solver'
 
-solver = Solver.new
+describe Solver do
+  before :each do
+    @solver = Solver.new
+  end
+  describe 'factorial method' do
+    it 'takes one argument and returns its factorial' do
+      fact = @solver.factorial(3)
 
-describe 'factorial method' do
-  it 'takes one argument and returns its factorial' do
-    fact = solver.factorial(3)
+      expect(fact).to eq(6)
+    end
 
-    expect(fact).to eq(6)
+    it 'raises exception for negative number' do
+      expect { @solver.factorial(-5) }.to raise_exception
+    end
   end
 
-  it 'raises exception for negative number' do
-    expect { fact.factorial(-5) }.to raise_exception
-  end
-end
+  describe 'reverse method' do
+    it 'takes a string and return it reversed' do
+      rev = @solver.reversed('world')
 
-describe 'reverse method' do
-  it 'takes a string and return it reversed' do
-    rev = solver.reversed('world')
-
-    expect(rev).to eq(rev)
-  end
-end
-
-describe 'fizzbuzz method' do
-  it 'takes an integer N and returns a string fizz if N is divisible by 3' do
-    str = solver.fizzbuzz(3)
-
-    expect(str).to eq('fizz')
+      expect(rev).to eq(rev)
+    end
   end
 
-  it 'takes an integer N and returns a string buzz if N is divisible by 5' do
-    str = solver.fizzbuzz(5)
+  describe 'fizzbuzz method' do
+    it 'takes an integer N and returns a string fizz if N is divisible by 3' do
+      str = @solver.fizzbuzz(3)
 
-    expect(str).to eq('buzz')
-  end
+      expect(str).to eq('fizz')
+    end
 
-  it 'takes an integer N and returns a string fizz if N is divisible by 3 and 5' do
-    str = solver.fizzbuzz(15)
+    it 'takes an integer N and returns a string buzz if N is divisible by 5' do
+      str = @solver.fizzbuzz(5)
 
-    expect(str).to eq('fizzbuzz')
+      expect(str).to eq('buzz')
+    end
+
+    it 'takes an integer N and returns a string fizz if N is divisible by 3 and 5' do
+      str = @solver.fizzbuzz(15)
+
+      expect(str).to eq('fizzbuzz')
+    end
   end
 end
